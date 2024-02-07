65062eba09d4ff585fe5fae5d14fb8423313e2b4
josephtiong <58130390+josephtiong@users.noreply.github.com>
2024-02-07 00:55:14 +0000

    renamed file

--- prime_factor.py
+++ prime_factor.py
@@ -2,10 +2,14 @@ from math import sqrt
 def find_prime(number):
     prime_factors = list()
 
+    #First step is to keep dividing the number with 2 until it becomes an odd number
+    #since all even numbers have 2 as prime factor
     while (number % 2 == 0):
         prime_factors.append(2)
         number = number/2
 
+    #Second step is to deal with odd numbers. We can start with smallest prime number
+    #and increase the prime numbers 
     candidate = 3
     max_factor = sqrt(number)
     while (candidate <=max_factor):
@@ -24,4 +28,4 @@ def find_prime(number):
 
     return prime_factors
 
-print(find_prime(786))
+print(find_prime(784))
